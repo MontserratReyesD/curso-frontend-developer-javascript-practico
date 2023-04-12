25fdb00e6b8b193b4bb0f96fa4d7b4b06220c80b
MontserratReyesD <monse.reyes959@gmail.com>
2023-04-11 20:25:12 -0600

    Dandole funcionalidad al carrito de compras clase13.html

--- main.js
+++ main.js
@@ -2,9 +2,48 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
-//Funcion que nos ayuda a mostrar o quitar el menu chiquito 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+//Para el menu hamburguesa
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
+//Para el menu carrito
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+
+//Aqui agregamos los eventos de click
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarrito);
+
+
+//Funcion que nos ayuda a mostrar o quitar el menu chiquito 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
+
+//Funcion que nos ayuda a mostrar o quitar el menu en mobile 
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+//Función para el carrito de compras.
+function toggleCarrito(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+}
+

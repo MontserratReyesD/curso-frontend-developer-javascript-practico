01b514133eed8906ba07e151ff32ad1a618cdf39
MontserratReyesD <monse.reyes959@gmail.com>
2023-04-12 19:32:09 -0600

    Agregando la lista de productos

--- main.js
+++ main.js
@@ -10,12 +10,13 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 
+//Para agregar todo el HTML a javaScript.
+const cardsContainer = document.querySelector('.cards-container');
 
 //Aqui agregamos los eventos de click
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarrito);
-
 
 //Funcion que nos ayuda a mostrar o quitar el menu chiquito 
 function toggleDesktopMenu(){
@@ -47,3 +48,67 @@ function toggleCarrito(){
     aside.classList.toggle('inactive');
 }
 
+//Creamos una lista de productos, donde se almacenaran los distintos productos que mostrara la página.
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Pantalla',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+productList.push({
+    name: 'Computadora',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+//Función que nos permite leer un array y mostrar los elementos en un HTML
+function renderProducts(arr){
+    //Leemos el array de los productos
+    //Cremoa elemento por elemento del HTML
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        //product = {name, price, image} => product.img
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+//Llamamos a la función y le pasamos el arreglo con el que queremos trabajar
+renderProducts(productList);
